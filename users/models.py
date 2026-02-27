045f99cb810a206fa56e8e9b1a4b8888949093b0
VaibhavvvMehta <vaibhavmehta605@gmail.com>
2026-02-27 16:40:09 +0530

    Initial Setup of Project done with Custom User

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    email=models.EmailField(unique=True)
+    bio=models.TextField(blank=True)
+    avtar=models.ImageField(upload_to='users/avatars/',blank=True,null=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+    
